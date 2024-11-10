8a1b462c8bfb0bb8adb45c340287fa3d206930c4
Joan Bellavista <jbb494@gmail.com>
2024-11-10 18:33:28 +0100

    fix: type tests fail results

--- src/result.spec.ts
+++ src/result.spec.ts
@@ -1,12 +1,12 @@
 import { describe, expect, it } from "@jest/globals";
 import { None, Some } from "./option";
-import { AsyncErr, AsyncOk, Err, Ok } from "./result";
+import { AsyncErr, AsyncOk, AsyncResult, Err, Ok, Result } from "./result";
 
 describe("Result", () => {
     describe("map", () => {
         it("map no async", () => {
             const start = Ok(12);
-            const result = start.map((x) => x + 1);
+            const result: Result<number, never> = start.map((x) => x + 1);
 
             expect(result.value).toBe(13);
             expect(result.error).toBeUndefined();
@@ -14,7 +14,7 @@ describe("Result", () => {
 
         it("map to async", async () => {
             const start = Ok(12);
-            const result = start.map((x) => {
+            const result: AsyncResult<number, never> = start.map((x) => {
                 return Promise.resolve(x + 1);
             });
 
@@ -26,10 +26,10 @@ describe("Result", () => {
 
         it("map to async and not async", async () => {
             const start = Ok(12);
-            const result1 = start.map((x): Promise<number> => {
+            const result1: AsyncResult<number, never> = start.map((x): Promise<number> => {
                 return new Promise((resolve) => resolve(x + 1));
             });
-            const result = result1.map((x) => x + 1);
+            const result: AsyncResult<number, never> = result1.map((x) => x + 1);
 
             expect(await result.value).toEqual(Some(14));
             expect((await result).value).toBe(14);
@@ -39,7 +39,7 @@ describe("Result", () => {
 
         it("promise to map", async () => {
             const start = AsyncOk(Promise.resolve(12));
-            const result = start.map((x) => x + 1);
+            const result: AsyncResult<number, never> = start.map((x) => x + 1);
 
             expect(await result.value).toEqual(Some(13));
             expect((await result).value).toBe(13);
@@ -49,22 +49,31 @@ describe("Result", () => {
 
         it("promise to async map to map", async () => {
             const start = AsyncOk(Promise.resolve(12));
-            const result1 = start.map((x) => {
+            const result1: AsyncResult<number, never> = start.map((x) => {
                 return Promise.resolve(x + 1);
             });
-            const result = result1.map((x) => x + 1);
+            const result: AsyncResult<number, never> = result1.map((x) => x + 1);
 
             expect(await result.value).toEqual(Some(14));
             expect((await result).value).toBe(14);
             expect(await result.error).toEqual(None);
             expect((await result).error).toBeUndefined();
         });
+
+        it("map to err should be err", async () => {
+            const start = Err(new Error("Error"));
+
+            const result: Result<never, Error> = start.map((x) => x + 1);
+
+            expect(result.value).toBeUndefined();
+            expect(result.error).toEqual(new Error("Error"));
+        });
     });
 
     describe("mapError", () => {
         it("er map", () => {
             const start = Err(12);
-            const result = start.map((x) => x + 1);
+            const result: Result<number, number> = start.map((x) => x + 1);
 
             expect(result.value).toBeUndefined();
             expect(result.error).toBe(12);
@@ -72,7 +81,7 @@ describe("Result", () => {
 
         it("mapError no async", () => {
             const start = Err(12);
-            const result = start.mapError((x) => x + 1);
+            const result: Result<never, number> = start.mapError((x) => x + 1);
 
             expect(result.value).toBeUndefined();
             expect(result.error).toBe(13);
@@ -80,7 +89,7 @@ describe("Result", () => {
 
         it("mapError to async", async () => {
             const start = Err(12);
-            const result = start.mapError((x) => {
+            const result: AsyncResult<never, number> = start.mapError((x) => {
                 return Promise.resolve(x + 1);
             });
 
@@ -92,7 +101,7 @@ describe("Result", () => {
 
         it("mapError to async and not async", async () => {
             const start = Err(12);
-            const result1 = start.mapError((x): Promise<number> => {
+            const result1: AsyncResult<never, number> = start.mapError((x): Promise<number> => {
                 return new Promise((resolve) => resolve(x + 1));
             });
             const result = result1.mapError((x) => x + 1);
@@ -105,7 +114,7 @@ describe("Result", () => {
 
         it("err promise to map", async () => {
             const start = AsyncErr(Promise.resolve(12));
-            const result = start.mapError((x) => x + 1);
+            const result: AsyncResult<never, number> = start.mapError((x) => x + 1);
 
             expect(await result.value).toEqual(None);
             expect((await result).value).toBeUndefined();
@@ -115,7 +124,7 @@ describe("Result", () => {
 
         it("err promise to async map to map", async () => {
             const start = AsyncErr(Promise.resolve(12));
-            const result1 = start.mapError((x) => {
+            const result1: AsyncResult<never, number> = start.mapError((x) => {
                 return Promise.resolve(x + 1);
             });
             const result = result1.mapError((x) => x + 1);
@@ -130,7 +139,7 @@ describe("Result", () => {
     describe("flatMap", () => {
         it("ok flatMap sync ok", () => {
             const start = Ok(12);
-            const result = start.flatMap((x) => Ok(x + 1));
+            const result: AsyncResult<number, never> = start.flatMap((x) => Ok(x + 1));
 
             expect(result.value).toBe(13);
             expect(result.error).toBeUndefined();
@@ -139,7 +148,9 @@ describe("Result", () => {
         it("async ok flatMap ok", async () => {
             const start = AsyncOk(Promise.resolve(12));
 
-            const result = start.flatMap((x) => Promise.resolve(Ok(x + 1)));
+            const result: AsyncResult<number, never> = start.flatMap((x) =>
+                Promise.resolve(Ok(x + 1)),
+            );
 
             expect(await result.value).toEqual(Some(13));
             expect(await result.error).toEqual(None);
@@ -153,9 +164,9 @@ describe("Result", () => {
         it("sync ok async flatMap ok", async () => {
             const start = Ok(12);
             const f = async (x: number) => Promise.resolve(Ok(x + 1));
-            const result = start.flatMap(f);
+            const result: AsyncResult<number, never> = start.flatMap(f);
 
-            const resultResolved = await result;
+            const resultResolved: Result<number, never> = await result;
 
             expect(resultResolved.value).toBe(13);
             expect(resultResolved.error).toBeUndefined();
